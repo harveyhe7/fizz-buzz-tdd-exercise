b48c57971e0c1118a7b8cf1c2d0bca238b736302
Harvey He <HEHA7@oocl.com>
2025-08-22 15:16:11 +0800

    refactor: 抽取getReturnResult方法

--- src/test/java/FizzBuzzGameTest.java
+++ src/test/java/FizzBuzzGameTest.java
@@ -6,38 +6,41 @@ public class FizzBuzzGameTest {
     @Test
     void given_number_when_countOff_then_return_itself() {
         // Given normal number
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-        // When countOff is called with 1
-        String result = fizzBuzzGame.countOff(1);
+        String result = getReturnResult(1);
+        // When countOff
         // Then it should return "1"
         assertEquals("1", result);
     }
 
     @Test
     void given_multiply_of_3_when_countOff_then_return_Fizz() {
-        // Given a number that is a multiple of 3
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-        // When countOff is called with 3
-        String result = fizzBuzzGame.countOff(3);
+        // Given
+        String result = getReturnResult(3);
+        // When countOff
         // Then it should return "Fizz"
         assertEquals("Fizz", result);
     }
+
     @Test
     void given_multiply_of_5_when_countOff_then_return_Buzz() {
-        // Given a number that is a multiple of 5
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-        // When countOff is called with 5
-        String result = fizzBuzzGame.countOff(5);
+        // Given
+        String result = getReturnResult(5);
+        // When countOff
         // Then it should return "Buzz"
         assertEquals("Buzz", result);
     }
+
     @Test
     void given_multiply_of_3_and_5_when_countOff_then_return_FizzBuzz() {
-        // Given a number that is a multiple of 3 and 5
-        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
-        // When countOff is called with 15
-        String result = fizzBuzzGame.countOff(15);
+        // Given
+        String result = getReturnResult(15);
+        // When countOff
         // Then it should return "FizzBuzz"
         assertEquals("FizzBuzz", result);
+    }
+
+    private String getReturnResult(int i) {
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        return fizzBuzzGame.countOff(i);
     }
 }
