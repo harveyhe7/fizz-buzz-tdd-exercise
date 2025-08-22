66f85635612dcf5c98a61e01b4db6be5706bcaea
Harvey He <HEHA7@oocl.com>
2025-08-22 15:03:05 +0800

    feat:增加输入为同时被五和3三整除的处理逻辑，即Task4

--- src/test/java/FizzBuzzGameTest.java
+++ src/test/java/FizzBuzzGameTest.java
@@ -31,4 +31,13 @@ public class FizzBuzzGameTest {
         // Then it should return "Buzz"
         assertEquals("Buzz", result);
     }
+    @Test
+    void given_multiply_of_3_and_5_when_countOff_then_return_FizzBuzz() {
+        // Given a number that is a multiple of 3 and 5
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        // When countOff is called with 15
+        String result = fizzBuzzGame.countOff(15);
+        // Then it should return "FizzBuzz"
+        assertEquals("FizzBuzz", result);
+    }
 }
