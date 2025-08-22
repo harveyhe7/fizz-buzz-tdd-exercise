696b9acd35bb91d6e019959df4c2165c03f92c13
Harvey He <HEHA7@oocl.com>
2025-08-22 14:56:29 +0800

    feat:增加输入为被三整除的处理逻辑，即Task2，补充单元测试的注释

--- src/main/java/FizzBuzzGame.java
+++ src/main/java/FizzBuzzGame.java
@@ -1,5 +1,8 @@
 public class FizzBuzzGame {
     public String countOff(int number) {
+        if(number % 3 == 0){
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzGameTest.java
+++ src/test/java/FizzBuzzGameTest.java
@@ -5,8 +5,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class FizzBuzzGameTest {
     @Test
     void given_number_when_countOff_then_return_itself() {
+        // Given normal number
         FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        // When countOff is called with 1
         String result = fizzBuzzGame.countOff(1);
+        // Then it should return "1"
         assertEquals("1", result);
+    }
+
+    @Test
+    void given_multiply_of_3_when_countOff_then_return_Fizz() {
+        // Given a number that is a multiple of 3
+        FizzBuzzGame fizzBuzzGame = new FizzBuzzGame();
+        // When countOff is called with 3
+        String result = fizzBuzzGame.countOff(3);
+        // Then it should return "Fizz"
+        assertEquals("Fizz", result);
     }
 }
